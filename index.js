f5c7af098982a4128b8da86393d6186aa510959e
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-11-22 14:27:24 -0500

    Created get, post, put, and delete request methods. (VPP)

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require("express");
+const repoContext = require("./MusicLibraryStarterCode/repository/repository-wrapper");
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 app.listen(3000, function () {
     console.log("Server started.  Listening on port 3000.");
 });
 
+app.get("/api/songs", (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+
+app.post("/api/songs", (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+});
+
+app.put("/api/songs/:id", (req, res) => {
+    const id = req.params.id;
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+    return res.send(updatedSong)
+});
+
+app.delete("/api/songs/:id", (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
+    return res.send(updatedDataSet);
+});
